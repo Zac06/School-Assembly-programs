81c4364a52245c1667b2a5112f86f65fc3a58f24
Zac06 <121717450+Zac06@users.noreply.github.com>
2023-03-01 13:07:11 +0100

    Add files via upload

--- sorting.s
+++ sorting.s
@@ -1,7 +1,7 @@
 valori:
     db 0x12
     db 0x34
-    db 0x7a
+    db 0x10
     db 0x13
     db 0x9b
     db 0x65
@@ -19,25 +19,41 @@ start:
     mov dh, offset ultimo   ;muovi la posizione (numero di elementi) dell'ultimo elemento in dh
     dec dh                  ;decrementa dh (ciclo esterno exchange sort)
     mov dl, offset ultimo   ;muovi la posizione (numero di elementi) dell'ultimo elemento in dh
-    cicloEst.
+    cicloEst:
         mov cl, ch          ;muovi in cl il contatore del ciclo esterno
+        inc cl
     ciclo:
         mov bx, 0           ;azzera i valori
-        mov si, ch          ;muovi nel puntatore 
+        mov ax, 0
+        mov al, ch
+        mov si, ax          ;muovi nel puntatore 
         mov bh, byte [si]   ;estrai il valore puntato dal ciclo esterno
-        mov si, cl          ;muovi nel puntatore
+        mov ax, 0
+        mov al, cl
+        mov si, ax          ;muovi nel puntatore
         mov bl, byte [si]   ;estrai il valore puntato dal ciclo interno
         cmp bh, bl
         ja scambia
         jmp continua
         scambia:
-            mov al, 0
+            mov ax, 0
             mov al, bh
             mov bh, bl
             mov bl, al
+            mov ax, 0
+            mov al, ch
+            mov si, ax
+            mov byte [si], bh
+            mov ax, 0
+            mov al, cl
+            mov si, ax
+            mov byte [si], bl
         continua:
             inc cl          ;incrementa cl (i+1)
             cmp cl, dl
             jbe ciclo
             inc ch
-            jmp ciclo
+            cmp ch, dh
+            jbe cicloEst
+            jmp end
+end:
